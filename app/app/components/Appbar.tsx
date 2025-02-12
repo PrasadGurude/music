4e6d5731b1b66fdf9e21c3db80783bfc1dcccba1
Prasad Gurude <prasadgurude76@gmail.com>
2025-02-13 01:15:36 +0530

    prisma schema writing is complit

--- app/app/components/Appbar.tsx
+++ app/app/components/Appbar.tsx
@@ -1,17 +1,17 @@
 "use client"
 
-import { signIn } from "next-auth/react"
+import { signIn, signOut, useSession } from "next-auth/react"
 
 export function Appbar() {
-  return (
-    <div className="flex justify-between mx-3">
-        <div className="self-center"> Muzi </div>
-        <div> 
-            <button className=" m-2 p-2 bg-blue-400 rounded-lg" onClick={()=>{
-                signIn()
-            }}>Signin</button>
+    const session = useSession();
+    return (
+        <div className="flex justify-between mx-3">
+            <div className="self-center"> Muzi </div>
+            <div>
+                {session.data?.user && <button className="m-2 p-2 bg-blue-400 rounded-lg" onClick={() => signOut()}>Logout</button>}
+                {!session.data?.user &&<button className=" m-2 p-2 bg-blue-400 rounded-lg" onClick={() => {signIn()}}>Signin</button>}
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
